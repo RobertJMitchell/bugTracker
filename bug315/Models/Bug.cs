a39c93523408bd3c128fee0296ef83b9533975ad
RobertJMitchell <rjrocks32@gmail.com>
2015-03-25 09:27:46 -0500

    added two new classes bug.cs and tasktodo.cs

---
+++ bug315/Models/Bug.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace bug315.Models
+{
+    public class Bug: WorkItem
+    {
+        public string StepsToReproduce { get; set; }
+    }
+}
