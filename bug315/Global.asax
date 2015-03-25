11a70faeb1da4e9630aad2c567a33da565c24b32
RobertJMitchell <rjrocks32@gmail.com>
2015-03-25 09:25:06 -0500

    base package

---
+++ bug315/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="bug315.MvcApplication" Language="C#" %>
---
+++ bug315/Models/WorkItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace bug315.Models
+{
+    public abstract class WorkItem
+    {
+        public string Title { get; set; }
+        public DateTime DateCreated { get; set; }
+        //1, 2, and 3 are the priority integers
+        public int Priority { get; set; }
+    }
+}
